adcf6fcf3e874779e0142552376df2c4c5f56be0
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-18 00:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let box=document.querySelector("div");
+let input=document.querySelector(".input");
+let button=document.querySelector(".btn");
+
+button.addEventListener("click",Onclick);
+function Onclick(){
+	box.classList.add("search");
+	// JavaScript code
+input.focus();
+
+}
+
